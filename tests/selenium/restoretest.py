9408f343b2d6bb95daa8a8fb29f959494a03406b
[Aron Schueler] <aron.schueler@dass-it.de>
2017-12-07 16:17:23 +0100

    Adds variables for restore testing

--- tests/selenium/restoretest.py
+++ tests/selenium/restoretest.py
@@ -7,6 +7,9 @@ from selenium.common.exceptions import NoSuchElementException, NoAlertPresentExc
 import unittest, time, re, sys, os
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 
 
 
@@ -25,8 +28,10 @@ class WebuiSeleniumTest(unittest.TestCase):
         
     def test_restore(self):        
         
-        # LOGGING IN:
+        
         driver = self.driver
+        actions = ActionChains(self.driver)
+        # LOGGING IN:
         driver.get(self.base_url + "/bareos-webui/auth/login")
         driver.find_element_by_xpath("//button[@type='button']").click()
         driver.find_element_by_xpath("//form[@id='login']/div/div/div/div/div[2]/div/ul/li[2]/a/span").click()
@@ -37,42 +42,40 @@ class WebuiSeleniumTest(unittest.TestCase):
         driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
         driver.find_element_by_link_text("English").click()
         driver.find_element_by_xpath("//input[@id='submit']").click()
+        
+        # CHANGING TO RESTORE TAB:
         self.click_and_wait("/bareos-webui/restore/")
+        
         
         
         # SELECTING CLIENT:
         # Selects the correct client
         self.click_element_and_wait("(//button[@type='button'])[3]")
-        self.click_element_and_wait("//form[@id='restore']/div/div/p/div/div/ul/li/a")
+        driver.find_element_by_css_selector("span.text").click()
+        # self.click_element_and_wait("/html/body/div[2]/div/div/form/div[1]/div[1]/p[1]/div/div/ul/li[1]/a/span[1]")
+        
+        # driver.find_element_by_link_text(client).click()
+        
+        
+        # CLICK ON FILE VIA XPATH-POSITION:
+        # to be replaced by click_element_and_wait
+        # self.click_element_and_wait("//a[contains(text(),'/')]")
+        # driver.find_element_by_xpath("//a[contains(text(),'/')]").click()
+        # driver.find_element_by_xpath("//a[contains(text(),'/')]").click()
+        # time.sleep(t)
+        element = self.wait_for_element(By.XPATH, "//a[contains(text(),'/')]")
+        # element = driver.find_element_by_xpath("//a[contains(text(),'/')]")
+        print "element %s - %s" % (element, dir(element))
+        
+        # actions.double_click(element)
+        # actions.perform()
+        element.send_keys(Keys.ARROW_RIGHT)
         time.sleep(t)
         
         
-        # CLICK ON FILE VIA CSS (broken):
-        # Clicks on css element and waits
-        # for them to load.
-        # driver.find_element_by_css_selector("i.jstree-icon.jstree-ocl").click()
-        # time.sleep(3)
-        # driver.find_element_by_css_selector("#-39 > i.jstree-icon.jstree-ocl").click()
-        # time.sleep(3)
-        # driver.find_element_by_css_selector("#-13 > i.jstree-icon.jstree-ocl").click()
-        # time.sleep(3)
-        # driver.find_element_by_css_selector("#620_anchor > i.jstree-icon.jstree-checkbox").click()
-        # time.sleep(3)
-        # self.click_css_and_wait("-39", "jstree-ocl")
-        # self.click_css_and_wait("-13", "jstree-ocl")
-        # self.click_css_and_wait("620_anchor", ".jstree-checkbox")     
-        
-        
-        # CLICK ON FILE VIA XPATH-POSITION
-        # to be replaced by click_element_and_wait
-        
-        driver.find_element_by_xpath("//i").click()
-        time.sleep(3)
-        driver.find_element_by_xpath("//li/ul/li/i").click()
-        time.sleep(3)
-        driver.find_element_by_xpath("//li/ul/li/ul/li/i").click()
-        time.sleep(3)
-        driver.find_element_by_xpath("//li[6]/a/i").click()
+        self.click_element_and_wait("//a[contains(text(),'etc/')]")
+        self.click_element_and_wait("//a[contains(text(),'bareos/')]")
+        self.click_element_and_wait("//a[contains(text(),'bconsole.conf')]")
         
         
         # CONFIRMATION:
@@ -87,87 +90,99 @@ class WebuiSeleniumTest(unittest.TestCase):
         driver.find_element_by_link_text(username).click()
         time.sleep(2)
         driver.find_element_by_link_text("Logout").click()
-        
-        
-        
-    
+                
+    # This Method clicks and URL and waits
+    # until the URL matches the desired URL.
     def click_and_wait(self, what):
-        # current time is catched
         start_time = time.time()
         i=0
         # the url to compare the current url against
         url=self.base_url + what
+        print url
         # xpath-reference is generated
         xpath="//a[contains(@href, '%s')]" % what
         # while the url is not the desired url
         # try to click on the xpath element
         # until the time is more than 10 seconds
         # and displays the time the test took.
-        while (url not in self.driver.current_url) or i<10:
+        while (url not in self.driver.current_url) and i<10:
             try:
-                self.driver.find_element_by_xpath(xpath).click()
+                self.driver.find_element_by_xpath(xpath)
             except (NoSuchElementException, ElementNotInteractableException) as e:
                 print "error %s" % i
+            else:
+                time.sleep(1)
+                print "Try %s succeeded" % what
+                self.driver.find_element_by_xpath(xpath).click()
+                return True
             time.sleep(1)
             i=i+1
             if(i==10):
                 print "Generated timeout."
-                return False
+                return True
         timer = time.time() - start_time
         print url + " loaded after %s seconds." % timer
         
     # Works like click_and_wait but also
     # works with non-url's: the xpath
     # must be given as argument when calling.
-    def click_element_and_wait(self, what):
+    def click_element_and_wait(self, xpath):
         i=0
-        while i<6 or False:
+        t=0
+        found=False
+        # while i<6 and found=False:
+        while i<6 and t==0:
+            time.sleep(2)
             try:
-                self.driver.find_element_by_xpath(what)
-            except (ElementNotInteractableException, NoSuchElementException) as e:
-                print "Error: %s of 6" % i
+                self.driver.find_element_by_xpath(xpath)
+            except ElementNotInteractableException:
+                print "Waiting since %s sec" % i
+            # except NoSuchElementException:
+           #      print "Waiting ince %s sec" % i
+           #      i=i+1
             else:
-                 time.sleep(1)
-                 print "Try %s succeeded" % what
-                 self.driver.find_element_by_xpath(what).click()
-                 return True
-            time.sleep(1)
+                 print "Try %s succeeded" % xpath
+                 self.driver.find_element_by_xpath(xpath).click()
+                 time.sleep(2)
+                 t=1
+                 # found=True
             i=i+1
             if(i==6):
-                print "Timeout while loading %s ." % what
-        print "Element loaded after %s seconds." %i
+                print "Timeout while loading %s ." % xpath
+                
+        if(i==6):
+            return False
+        else:    
+            print "Element loaded after %s seconds." %i
 
-    def click_css_and_wait(self, what, where):
-        print what
-        i=0
-        css = "#%s > i.jstree-icon." % what
-        css = css + "%s" % where
-        print css
-        while i<4:
+    def wait_for_element(self, by, value):
+        i=10
+        element=None
+        # while i<6 and found=False:
+        while i>0 and element is None:
             try:
-                self.driver.find_element_by_css_selector(css).click()
-            except InvalidSelectorException:
-                print "error %s" % (i+1)
+                element = self.driver.find_element(by, value)
+            except ElementNotInteractableException:
+                print "Waiting since %s sec" % i
                 time.sleep(1)
-            i=i+1
-            if(i==4):
-                print "Timeout while loading %s ." % css
-        print "Element loaded after %s seconds." %i
+            except NoSuchElementException:
+                print "NSE Waiting ince %s sec" % i                
+                time.sleep(1)
+            i=i-1
+                
+        if(i==0):
+            print "Timeout while loading %s ." % xpath
+            return None
+        else:    
+            print "Element loaded after %s seconds." %i
+            return element
+
         
-    def wait_for_jQuery(self):
-        i=0
-        while (self.driver.execute_script("return jQuery.active == 0") and i<10):
-            print "loading..."
-            time.sleep(1)
-            i=i+1
-            if i==10:
-                return False
-        return True
-        
-    def wait_for_element(self, what):
-        element = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, what))
-        )            
+    # def wait_for_element(self, what):
+    #     element = WebDriverWait(driver, 10).until(
+    #         EC.element_to_be_clickable((By.XPATH, what))
+    #     )
+
     def is_alert_present(self):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
@@ -192,6 +207,7 @@ if __name__ == "__main__":
 
     # get username from environment if set
     # otherwise use defaults
+    client = "bareos-fd"
     username = os.environ.get('USERNAME')
     password = os.environ.get('PASSWORD')
     if not username:
